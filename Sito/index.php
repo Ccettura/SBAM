7e751dcdc1404086dcdba0fe085de4a948b9b2a2
Antonello <antonello.lagalante.9@gmail.com>
2021-05-09 13:49:33 +0200

    Versione 0

---
+++ Sito/index.php
@@ -0,0 +1,48 @@
+<?php
+    /*! Codice PHP */
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>SBAM</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" integrity="sha512-NmLkDIU1C/C88wi324HBc+S2kLhi08PN5GDeUVVVC/BVt/9Izdsc9SVeVfA1UZbY3sHUlDSyRXhCzHfr6hmPPw==" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="stile.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+    <div class="header">
+        <div class="contenuto_header">
+            <div class="logo">
+                <img src="logo.png" alt="200" height="44">
+            </div>
+
+            <div class="divisore"></div>
+
+            <ul class="menu">
+                <li><a href="">Home</a></li>
+                <li><a href="">Lista</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contatti</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="background"></div>
+    <div class="logoSfondo"></div>
+
+    <div class="cerca">
+        <h1 class="titoli titoli">CERCA UN LIBRO O UN FUMETTO</h1>
+    </div>
+
+
+
+</body>
+</html>
