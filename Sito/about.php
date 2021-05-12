9d7533978b778e44668089138d6fb11dd5eaafb6
Antonello <antonello.lagalante.9@gmail.com>
2021-05-12 22:35:42 +0200

    Versione 0

--- Sito/about.php
+++ Sito/about.php
@@ -4,12 +4,31 @@ include 'header.php';
 ?>
 
 
+<div class="copertina_about">
+    <h1 class="giallo about">ABOUT</h1>
+    <h2 class="bianco who">WHO WE ARE</h2>
+</div>
 
+<div class="griglia">
 
+    <div class="riga1">
+        <div class="elemento1">
 
+        </div>
+        <div class="elemento2">
 
+        </div>
+    </div>
 
+    <div class="riga2">
+        <div class="elemento3">
 
+        </div>
+        <div class="elemento4">
+
+        </div>
+    </div>
+</div>
 
 
 <?php
