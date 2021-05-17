3ac526f111fe420ce074384431120b37076e18ce
Antonello <antonello.lagalante.9@gmail.com>
2021-05-17 19:22:55 +0200

    Versione 1

--- Sito/about.php
+++ Sito/about.php
@@ -9,25 +9,32 @@ include 'header.php';
     <h2 class="bianco who">WHO WE ARE</h2>
 </div>
 
-<div class="griglia">
+<div class="griglia mt1">
 
-    <div class="riga1">
-        <div class="elemento1">
-
+    <div class="riga">
+        <div class="elemento">
+            <div class="immagine1"></div>
+            <h1 class="giallo sottotitoli mt">Antonello Lagalante</h1>
+            <p class="bianco paragrafi">Descrizione.</p>
         </div>
-        <div class="elemento2">
-
+        <div class="elemento">
+            <div class="immagine2"></div>
+            <h1 class="giallo sottotitoli">Francesco Accettura</h1>
+            <p class="bianco paragrafi">Descrizione.</p>
+        </div>
+        <div class="elemento">
+            <div class="immagine3"></div>
+            <h1 class="giallo sottotitoli">Domenico Scorcia</h1>
+            <p class="bianco paragrafi">Descrizione.</p>
+        </div>
+        <div class="elemento">
+            <div class="immagine4"></div>
+            <h1 class="giallo sottotitoli">Nicolò Andrea Carissimo</h1>
+            <p class="bianco paragrafi">Descrizione.</p>
         </div>
     </div>
 
-    <div class="riga2">
-        <div class="elemento3">
 
-        </div>
-        <div class="elemento4">
-
-        </div>
-    </div>
 </div>
 
 
